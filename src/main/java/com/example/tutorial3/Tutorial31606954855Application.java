06b1d22d4af8cfc3b15681b2f0d3b6abf2117731
Laborawaty Rajagukguk <laborawaty@bizzy.co.id>
2018-02-27 23:03:36 +0700

    tutorial 3

---
+++ src/main/java/com/example/tutorial3/Tutorial31606954855Application.java
@@ -0,0 +1,12 @@
+package com.example.tutorial3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial31606954855Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tutorial31606954855Application.class, args);
+	}
+}
